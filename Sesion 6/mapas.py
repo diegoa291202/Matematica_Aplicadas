aa3a6fc5b086f8895f79d19e3f1519eb45e27481
Your Name <you@example.com>
2021-05-04 21:28:07 -0500

    commit

--- Sesion 6/mapas.py
+++ Sesion 6/mapas.py
@@ -1,3 +1,3 @@
 import folium
-m1 = folium.Map(location=[41.387, 2.172659], tiles='openstreetmap', zoom_start=18)
+m1 = folium.Map(location=[-9.189967, -75.015152], tiles='openstreetmap', zoom_start=18)
 m1.save('mapa1.html')
